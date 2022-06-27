363ff215dba0772568e968c4584bb86a04226269
Kostya <konstantin12.072@gmail.com>
2022-06-27 17:33:21 +0300

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<div class="footer-section footer-section1">
+    <h2><i class="fas fa-user-alt"></i> Социальные Сети</h2>
+    <div class="footer-section-inner-container">
+        <span><a href="https://ru-ru.facebook.com/elonreevesmusk"><i class="fab fa-lg fa-facebook-square"></i> Facebook</a></span>
+        <span><a href="https://twitter.com/elonmusk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-lg fa-twitter-square"></i> Twitter</a></span>
+        <span><a href="https://www.instagram.com/tina_baeva_/"></i> Instagram</a></span>
+    </div>
+</div>
+
+<div class="footer-section footer-section2">
+    <h2><i class="fas fa-map-marked"></i> Свяжитесь с нами </h2>
+    <div class="footer-section-inner-container">
+        <p>Cвяжитесь с нами , что бы сделать наш сервис лучше, любой отзыв будет учтен.</p>
+        <p>Телефон: +2 010 11 39 11 48</p>
+        <p>E-mail: AscoDS@gmail.com</p>
+    </div>
+</div>
+<div class="footer-section  footer-section3">
+    <p>© 2022 AscoDS. Created by Myself. </p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a>Contact </a>
+</div>
