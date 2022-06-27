c8004c344b8587ad63413259a4c67bfce8e829c3
Kostya <konstantin12.072@gmail.com>
2022-06-27 17:32:26 +0300

    Create adminfranchises.php

---
+++ admin/adminfranchises.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Dashboard</title>
+    <link rel="icon" type="image/png" href="../img/logo.png">
+    <link rel="stylesheet" href="../style/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    $link = mysqli_connect("localhost", "root", "", "insurance");
+    $sql = "SELECT * FROM franchase";
+    $bookingsNo=mysqli_num_rows(mysqli_query($link, $sql));
+    $messagesNo=mysqli_num_rows(mysqli_query($link, "SELECT * FROM feedbackTable"));
+    $moviesNo=mysqli_num_rows(mysqli_query($link, "SELECT * FROM movieTable"));
+    $franchaseNo=mysqli_num_rows(mysqli_query($link, "SELECT * FROM franchase"));
+    ?>
+    <div class="admin-section-header">
+        <div class="admin-logo">
+            AscoDS
+        </div>
+        <div class="admin-login-info">
+            <i class="far fa-bell admin-notification-button"></i>
+            <i class="far fa-comment-alt"></i>
+            <a href="#">Welcome, Admin</a>
+            <img class="admin-user-avatar" src="../img/avatar.png" alt="">
+        </div>
+    </div>
+    <div class="admin-container">
+        <div class="admin-section admin-section1 ">
+            <ul>
+                <a href="adminfeedbacks.php"><li><i class="fas fa-sliders-h"></i>Отзывы <i class="fas admin-dropdown fa-chevron-right" ></i></li></a>
+                <a href="admin.php"><li><i class="fas fa-ticket-alt"></i>Заявки <i class="fas admin-dropdown fa-chevron-right"></i></li></a>
+                <a href="admin.php"><li class="admin-navigation-schedule"><div class="schedule-item schedule-item-selected"><i class="fas fa-calendar-alt"></i>Франшизы </div>
+                </li></a>
+
+                <a href="adminservices.php"><li><i class="fas fa-film"></i>Услуги <i class="fas admin-dropdown fa-chevron-right"></i></li></a>
+            </ul>
+        </div>
+        <div class="admin-section admin-section2">
+            <div class="admin-section-column">
+                <div class="admin-section-panel admin-section-stats">
+                    <div class="admin-section-stats-panel">
+                        <i class="fas fa-ticket-alt" style="background-color: #cf4545"></i>
+                        <h2 style="color: #cf4545"><?php echo $bookingsNo ?></h2>
+                        <h3>Заявок</h3>
+                    </div>
+                    <div class="admin-section-stats-panel">
+                        <i class="fas fa-film" style="background-color: #4547cf"></i>
+                        <h2 style="color: #4547cf"><?php echo $moviesNo ?></h2>
+                        <h3>Услуг</h3>
+                    </div>
+                    <div class="admin-section-stats-panel">
+                        <i class="fas fa-ticket-alt" style="background-color: #bb3c95"></i>
+                        <h2 style="color: #bb3c95"><?php echo $franchaseNo ?></h2>
+                        <h3>Франшиз</h3>
+                    </div>
+                    <div class="admin-section-stats-panel" style="border: none">
+                        <i class="fas fa-envelope" style="background-color: #3cbb6c"></i>
+                        <h2 style="color: #3cbb6c"><?php echo $messagesNo ?></h2>
+                        <h3>Отзывов</h3>
+                    </div>
+                </div>
+                <div class="admin-section-panel admin-section-panel1">
+                    <div class="admin-panel-section-header">
+                        <h2>Франшизы</h2>
+                        <i class="fas fa-ticket-alt" style="background-color: #cf4545"></i>
+                    </div>
+                    <div class="admin-panel-section-content">
+                        <?php
+                        if($result = mysqli_query($link, $sql)){
+                            if(mysqli_num_rows($result) > 0){
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<div class=\"admin-panel-section-booking-item\">\n";
+                                    echo "                            <div class=\"admin-panel-section-booking-response\">\n";
+                                    echo "                                <a href='deletefranchises.php?id=".$row['id']."'><i class=\"fas fa-check accept-booking\" title=\"Reject booking\"></i></a>\n";
+                                    echo "                                <a href='deletefranchises.php?id=".$row['id']."'><i class=\"fas fa-times decline-booking\" title=\"Reject booking\"></i></a>\n";
+                                    echo "                            </div>\n";
+                                    echo "                            <div class=\"admin-panel-section-booking-info\">\n";
+                                    echo "                                <div>\n";
+                                    echo "                                    <h3>". $row['name'] ."</h3>\n";
+                                    echo "                                    <i class=\"fas fa-circle \"></i>\n";
+                                    echo "                                    <h4>". $row['sname'] ."</h4>\n";
+                                    echo "                                    <i class=\"fas fa-circle \"></i>\n";
+                                    echo "                                    <h4>". $row['company'] ."</h4>\n";
+                                    echo "                                    <i class=\"fas fa-circle \"></i>\n";
+                                    echo "                                    <h4>". $row['position'] ."</h4>\n";
+                                    echo "                                </div>\n";
+                                    echo "                                <div>\n";
+                                    echo "                                    <h4>". $row['country'] ." ". $row['phone'] ."</h4>\n";
+                                    echo "                                    <i class=\"fas fa-circle\"></i>\n";
+                                    echo "                                    <h4>". $row['email'] ."</h4>\n";
+                                    echo "                                </div>\n";
+                                    echo "                            </div>\n";
+                                    echo "                        </div>";
+                                }
+                                mysqli_free_result($result);
+                            } else{
+                                echo '<h4 class="no-annot">No Franchises right now</h4>';
+                            }
+                        } else{
+                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                        }
+                        ?>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <script src="../scripts/jquery-3.3.1.min.js "></script>
+    <script src="../scripts/owl.carousel.min.js "></script>
+    <script src="../scripts/script.js "></script>
+</body>
+
+</html>
