4978e86b51d4d65247d86fccb5445d3769acab03
Kostya <konstantin12.072@gmail.com>
2022-06-27 17:33:23 +0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<div class="navbar-brand">
+    <a href="index.php">
+        <h1 class="navbar-heading">Asco DS</h1>
+    </a>
+</div>
+<div class="navbar-container">
+    <nav class="navbar">
+        <ul class="navbar-menu">
+            <li><a href="index.php">Главная</a></li>
+            <li><a href="schedule.php">О компании</a></li>
+            <li><a href="franchase.php">Франшиза</a></li>
+            <li><a href="contact-us.php">Отзыв</a></li>
+        </ul>
+    </nav>
+</div>
